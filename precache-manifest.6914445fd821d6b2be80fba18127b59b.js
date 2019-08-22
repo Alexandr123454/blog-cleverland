0fabfbc9407eaaa36a4ecc2082e638d5f9d63527
alexandr <alexandrov6927@gmail.com>
2019-08-23 00:45:37 +0300

    Updates

---
+++ precache-manifest.6914445fd821d6b2be80fba18127b59b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b72a849b0fb21eb8efcf8da2aa3c36d",
+    "url": "/blog-cleverland/index.html"
+  },
+  {
+    "revision": "49f4ce54878301fef7f4",
+    "url": "/blog-cleverland/static/css/main.dfc01444.chunk.css"
+  },
+  {
+    "revision": "b4b73601010409df91b9",
+    "url": "/blog-cleverland/static/js/2.09cda2a4.chunk.js"
+  },
+  {
+    "revision": "49f4ce54878301fef7f4",
+    "url": "/blog-cleverland/static/js/main.db8186c4.chunk.js"
+  },
+  {
+    "revision": "2e6d61f46d430860dc86",
+    "url": "/blog-cleverland/static/js/runtime~main.7ebfc6f8.js"
+  }
+]);
